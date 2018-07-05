c90efbcf0099bbab6b256f5203eeb7551cba41c5
Jacob Brunson <jacob@brunson.me>
2018-07-05 14:42:51 -0500

    Implemented AddressDAO, CreditCardDAO, and create method of CustomerDAO

--- src/cs4347/jdbcProject/ecomm/dao/impl/AddressDaoImpl.java
+++ src/cs4347/jdbcProject/ecomm/dao/impl/AddressDaoImpl.java
@@ -10,32 +10,30 @@ public class AddressDaoImpl implements AddressDAO
 {
 
     private static final String insertSQL =
-            "INSERT INTO CUSTOMER (first_name, last_name, dob, gender, email) VALUES (?, ?, ?, ?, ?);";
+            "INSERT INTO Address (address1, address2, city, state, zipcode, customer_id) VALUES (?, ?, ?, ?, ?, ?);";
+
+    private static final String retrieveSQL =
+            "SELECT address1, address2, city, state, zipcode, customer_id FROM Address WHERE customer_id = ? LIMIT 1;";
+
+    private static final String deleteSQL =
+            "DELETE Address WHERE customer_id = ?;";
 
 
     @Override
     public Address create(Connection connection, Address address, Long customerID) throws SQLException, DAOException {
-        if (address.getId() != null) {
-            throw new DAOException("Trying to insert Address with NON-NULL ID");
-        }
-
-        Connection connection = dataSource.getConnection();
         PreparedStatement ps = null;
         try {
-            ps = connection.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, customer.getFirstName());
-            // Set other statement attributes here...
+            ps = connection.prepareStatement(insertSQL);
+            ps.setString(1, address.getAddress1());
+            ps.setString(2, address.getAddress2());
+            ps.setString(3, address.getCity());
+            ps.setString(4, address.getState());
+            ps.setString(5, address.getZipcode());
+            ps.setLong(6, customerID);
             ps.executeUpdate();
 
-            // REQUIREMENT: Copy the auto-increment primary key to the customer ID.
-            ResultSet keyRS = ps.getGeneratedKeys();
-            keyRS.next();
-            int lastKey = keyRS.getInt(1);
-            customer.setId((long) lastKey);
-
-            return customer;
-        }
-        finally {
+            return address;
+        } finally {
             if (ps != null && !ps.isClosed()) {
                 ps.close();
             }
@@ -47,11 +45,47 @@ public class AddressDaoImpl implements AddressDAO
 
     @Override
     public Address retrieveForCustomerID(Connection connection, Long customerID) throws SQLException, DAOException {
-        return null;
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(retrieveSQL);
+            ps.setLong(1, customerID);
+            ResultSet result = ps.executeQuery();
+
+            Address address = null;
+
+            if (result.next()) {
+                address.setAddress1(result.getString(1));
+                address.setAddress2(result.getString(2));
+                address.setCity(result.getString(3));
+                address.setState(result.getString(4));
+                address.setZipcode(result.getString(5));
+            }
+
+            return address;
+        } finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
     }
 
     @Override
     public void deleteForCustomerID(Connection connection, Long customerID) throws SQLException, DAOException {
-
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(deleteSQL);
+            ps.setLong(1, customerID);
+            ps.executeUpdate();
+        } finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
     }
 }
--- src/cs4347/jdbcProject/ecomm/dao/impl/CreditCardDaoImpl.java
+++ src/cs4347/jdbcProject/ecomm/dao/impl/CreditCardDaoImpl.java
@@ -1,9 +1,90 @@
 package cs4347.jdbcProject.ecomm.dao.impl;
 
 import cs4347.jdbcProject.ecomm.dao.CreditCardDAO;
+import cs4347.jdbcProject.ecomm.entity.Address;
+import cs4347.jdbcProject.ecomm.entity.CreditCard;
+import cs4347.jdbcProject.ecomm.util.DAOException;
+
+import java.sql.*;
 
 
 public class CreditCardDaoImpl implements CreditCardDAO
 {
 
+    private static final String insertSQL =
+            "INSERT INTO CreditCard (name, number, expiration, securityCode, customer_id) VALUES (?, ?, ?, ?, ?);";
+
+    private static final String retrieveSQL =
+            "SELECT name, number, expiration, securityCode, customer_id FROM CreditCard WHERE customer_id = ? LIMIT 1;";
+
+    private static final String deleteSQL =
+            "DELETE CreditCard WHERE customer_id = ?;";
+
+    @Override
+    public CreditCard create(Connection connection, CreditCard creditCard, Long customerID) throws SQLException, DAOException {
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(insertSQL);
+            ps.setString(1, creditCard.getName());
+            ps.setString(2, creditCard.getCcNumber());
+            ps.setString(3, creditCard.getExpDate()); //TODO: probably need to convert to java.sql.Date
+            ps.setString(4, creditCard.getSecurityCode());
+            ps.setLong(5, customerID);
+            ps.executeUpdate();
+
+            return creditCard;
+        } finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public CreditCard retrieveForCustomerID(Connection connection, Long customerID) throws SQLException, DAOException {
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(retrieveSQL);
+            ps.setLong(1, customerID);
+            ResultSet result = ps.executeQuery();
+
+            CreditCard card = null;
+
+            if (result.next()) {
+                card.setName(result.getString(1));
+                card.setCcNumber(result.getString(2));
+                card.setExpDate(result.getString(3));
+                card.setSecurityCode(result.getString(4));
+            }
+
+            return card;
+        } finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public void deleteForCustomerID(Connection connection, Long customerID) throws SQLException, DAOException {
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(deleteSQL);
+            ps.setLong(1, customerID);
+            ps.executeUpdate();
+        } finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
 }
